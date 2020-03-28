26bdd51095ed036e05084f543905a31d2a4b63a5
DevashishRanjanGLA <devashish.ranjan_cs18@gla.ac.in>
2020-03-28 17:11:07 +0530

    initial commit

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,19 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("dev", 1);
+        obj.insert("rick", 4);
+        obj.insert("jain", 3);
+        obj.insert("xyz", 2);
+        obj.insert("abc", 5);
+        obj.display();
+    }
 }
